0152f1e4f6faf8446c8d0cc3ce4d49a56a3f300e
misfarsiddeek95 <asmmisfar@gmail.com>
2026-08-30 23:23:24 +0530

    Graph title font size changed.

--- src/components/TailorMadeForYou/ChartSection.tsx
+++ src/components/TailorMadeForYou/ChartSection.tsx
@@ -12,7 +12,7 @@ export default function ChartSection({ title, exporting, children }: ChartSectio
   return (
     <section className="border-b border-brand-main/10 pb-10 mb-10 last:border-b-0">
       <div className="flex flex-col md:flex-row md:relative items-center justify-center w-full gap-3 md:gap-0 mb-6">
-        <h2 className="font-heading text-[26px] font-normal leading-normal text-brand-main text-center">
+        <h2 className="font-heading text-3xl md:text-4xl font-normal leading-normal text-brand-main text-center">
           {title}
         </h2>
         <div className="flex items-center gap-2 md:absolute md:right-0">
